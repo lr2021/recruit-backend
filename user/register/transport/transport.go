a7370b3ec881c6afb0e3ae681bca8efa6b18e996
root <root@LAPTOP-FQL3FC52.localdomain>
2021-08-21 17:35:33 +0800

    service registry finished

--- user/register/transport/transport.go
+++ user/register/transport/transport.go
@@ -6,12 +6,66 @@ import (
 	"github.com/lr2021/recruit-backend/general/errors"
 	"github.com/lr2021/recruit-backend/user/register/model"
 	"net/http"
+	"regexp"
 )
+
+func CheckLoginRequest(req model.LoginRequest) bool {
+	if len(req.Username) == 0 && len(req.Tel) == 0 {
+		return false
+	}
+	if len(req.Username) > 0 && !regexp.MustCompile(`^[0-9a-zA-Z]{6,20}$`).MatchString(req.Username) {
+		return false
+	}
+	if !regexp.MustCompile(`^[0-9a-zA-Z]{6,20}$`).MatchString(req.Password) {
+		return false
+	}
+	if len(req.Tel) > 0 && !regexp.MustCompile(`^((0\d{2,3}-\d{7,8})|(1[358479]\d{9}))$`).MatchString(req.Tel) {
+		return false
+	}
+	return true
+}
+
+func CheckRegisterRequest(req model.RegisterRequest) bool {
+	if req.Password != req.RepeatedPassword {
+		return false
+	}
+	if !regexp.MustCompile(`^[0-9a-zA-Z]{6,20}$`).MatchString(req.Username) {
+		return false
+	}
+	if !regexp.MustCompile(`^[0-9a-zA-Z]{6,20}$`).MatchString(req.Password) {
+		return false
+	}
+	if !regexp.MustCompile(`^((0\d{2,3}-\d{7,8})|(1[358479]\d{9}))$`).MatchString(req.Tel) {
+		return false
+	}
+
+	return true
+}
+
+func CheckLogoutRequest(req model.LogoutRequest) bool {
+	return true
+}
+
+func CheckGetUserSolvedRequest(req model.GetUserSolvedRequest) bool {
+	return true
+}
+
+func CheckGetUserProfileRequest(req model.GetUserProfileRequest) bool {
+	return true
+}
+
+func CheckUpdateUserProfileRequest(req model.UpdateUserProfileRequest) bool {
+	return true
+}
+
+func CheckGetUserRankRequest(req model.GetUserRankRequest) bool {
+	return true
+}
 
 func DecodeLoginRequest(ctx context.Context, r *http.Request) (interface{}, error) {
 	var req model.LoginRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
+	if err != nil || !CheckLoginRequest(req) {
 		return nil, errors.BadRequest("para:001", "missing or invalid parameters")
 	}
 	return req, nil
@@ -20,7 +74,7 @@ func DecodeLoginRequest(ctx context.Context, r *http.Request) (interface{}, erro
 func DecodeRegisterRequest(ctx context.Context, r *http.Request) (interface{}, error) {
 	var req model.RegisterRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
+	if err != nil || !CheckRegisterRequest(req) {
 		return nil, errors.BadRequest("para:001", "missing or invalid parameters")
 	}
 	return req, nil
@@ -29,7 +83,7 @@ func DecodeRegisterRequest(ctx context.Context, r *http.Request) (interface{}, e
 func DecodeLogoutRequest(ctx context.Context, r *http.Request) (interface{}, error) {
 	var req model.LogoutRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
+	if err != nil || !CheckLogoutRequest(req) {
 		return nil, errors.BadRequest("para:001", "missing or invalid parameters")
 	}
 	return req, nil
@@ -38,7 +92,7 @@ func DecodeLogoutRequest(ctx context.Context, r *http.Request) (interface{}, err
 func DecodeGetUserSolvedRequest(ctx context.Context, r *http.Request) (interface{}, error) {
 	var req model.GetUserSolvedRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
+	if err != nil || !CheckGetUserSolvedRequest(req) {
 		return nil, errors.BadRequest("para:001", "missing or invalid parameters")
 	}
 	return req, nil
@@ -47,7 +101,7 @@ func DecodeGetUserSolvedRequest(ctx context.Context, r *http.Request) (interface
 func DecodeGetUserProfileRequest(ctx context.Context, r *http.Request) (interface{}, error) {
 	var req model.GetUserProfileRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
+	if err != nil || !CheckGetUserProfileRequest(req) {
 		return nil, errors.BadRequest("para:001", "missing or invalid parameters")
 	}
 	return req, nil
@@ -56,17 +110,16 @@ func DecodeGetUserProfileRequest(ctx context.Context, r *http.Request) (interfac
 func DecodeUpdateUserProfileRequest(ctx context.Context, r *http.Request) (interface{}, error) {
 	var req model.UpdateUserProfileRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
+	if err != nil || !CheckUpdateUserProfileRequest(req) {
 		return nil, errors.BadRequest("para:001", "missing or invalid parameters")
 	}
 	return req, nil
 }
 
-
 func DecodeGetUserRankRequest(ctx context.Context, r *http.Request) (interface{}, error) {
-	var req model.UpdateUserProfileRequest
+	var req model.GetUserRankRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
+	if err != nil || !CheckGetUserRankRequest(req) {
 		return nil, errors.BadRequest("para:001", "missing or invalid parameters")
 	}
 	return req, nil
